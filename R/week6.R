dfee922e9c859245247812f9c5bb0857c65cf6d0
Amanda Jensen <jens2482@umn.edu>
2024-02-24 23:34:12 -0600

    All tasks completed

--- R/week6.R
+++ R/week6.R
@@ -6,7 +6,20 @@ library(tidyverse)
 
 
 # Data Import
-citations <- stri_read_lines("../data/citations.txt", encoding = "UTF-16")
-citations_txt <- str_subset(citations, pattern = "^\\S+$")
+citations <- stri_read_lines("../data/citations.txt", encoding = "CP-1250")
+citations_txt <- str_subset(citations, pattern = "\\S+")
 print(length(citations)-length(citations_txt))
 print(mean(nchar(citations_txt)))
+
+# Data Cleaning
+sample_n(citations_tbl, size = 20)
+
+citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_remove_all(cite, pattern = "[\"']")) %>%
+  mutate(year = str_extract(cite, pattern = "\\d{4}")) %>%
+  mutate(page_start = str_extract(cite, pattern = "\\d{1,4}(?=-)")) %>%
+  mutate(perf_ref = grepl("[Pp]erformance", cite)) %>%
+  mutate(title = str_extract(cite, pattern = "(?<=\\). )\\b(?:\\S+? )*\\S+?\\. ")) %>%
+  mutate(first_author = str_extract(cite, pattern = "^\\w*, \\w\\.(?: \\w\\.)?"))
+
+print(sum(citations_tbl$first_author != "", na.rm = TRUE))
