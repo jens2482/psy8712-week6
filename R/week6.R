c64af418c059737d63bbf80f689068699f38b5c5
Amanda Jensen <jens2482@umn.edu>
2024-02-28 17:04:07 -0600

    Changes made during lecture

--- R/week6.R
+++ R/week6.R
@@ -6,20 +6,24 @@ library(tidyverse)
 
 
 # Data Import
-citations <- stri_read_lines("../data/citations.txt", encoding = "CP-1250")
+citations <- stri_read_lines("../data/citations.txt", encoding = "ISO-8859-1")
 citations_txt <- str_subset(citations, pattern = "\\S+")
+#citations_txt <- citations[!stri_isempty(citations)] #alternate to code above (preferred as it does not use regex)
 print(length(citations)-length(citations_txt))
 print(mean(nchar(citations_txt)))
 
 # Data Cleaning
-sample_n(citations_tbl, size = 20)
-
+sample_n(citations_tbl, size = 20) %>%
+  View
 citations_tbl <- tibble(line = seq_along(citations_txt), cite = citations_txt) %>%
+#citations_tbl <- tibble(line = 1:length(citations_txt), cite - citations_txt) %>% #alternate text to row above
   mutate(cite = str_remove_all(cite, pattern = "[\"']")) %>%
-  mutate(year = str_extract(cite, pattern = "\\d{4}")) %>%
-  mutate(page_start = str_extract(cite, pattern = "\\d{1,4}(?=-)")) %>%
-  mutate(perf_ref = grepl("[Pp]erformance", cite)) %>%
-  mutate(title = str_extract(cite, pattern = "(?<=\\)\\.*? )\\b(?:\\S+? )*\\S+?[\\.\?] ")) %>%
-  mutate(first_author = str_extract(cite, pattern = "^\\w*, \\w\\.(?: \\w\\.)?"))
+  mutate(year = as.integer(str_extract(cite, pattern = "\\d{4}"))) %>% #needed to change values to integers
+  mutate(page_start = as.integer(str_extract(cite, pattern = "\\d{1,4}(?=-)"))) %>%
+  #str_match(cite, "(\\d+_-\\d+ [,2])) # alternative function to line above
+  mutate(perf_ref = str_detect(str_to_lower(cite), "performance")) %>%
+  mutate(title = str_match(cite, "\\d{4}\\)\\. ([^\\.]+)")[,2]) %>%
+  mutate(first_author = str_extract(cite, "^\\w+, \\w\\.? ?\\w?\\.? ?\\w?\\."))
 
 print(sum(citations_tbl$first_author != "", na.rm = TRUE))
+#sum(!is.na(citations_tbl$first_author))  #simpler way
